90fb87341c0c00fc1e6ebb82ca9630c811ddfd6e
Nicolas Moreira Ferreira <126179384+nicolasmoreiraferreira@users.noreply.github.com>
2023-07-31 14:41:56 -0300

    safe

---
+++ aula13/ambiente.js
@@ -0,0 +1,22 @@
+var c = 1
+do {
+  console.log(`Passo ${c}`)
+  c++
+} while (c <= 6)
+
+/*
+var c = 1
+while (c <= 500) {
+  console.log(`Passo ${c}`)
+  c++
+}
+*/
+
+/*
+consone.log(`Tudo bem?`)
+consone.log(`Tudo bem?`)
+consone.log(`Tudo bem?`)
+consone.log(`Tudo bem?`)
+consone.log(`Tudo bem?`)
+consone.log(`Tudo bem?`)
+*/ 
