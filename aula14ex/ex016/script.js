34f9c5cd8ef9c3ae7d5e94739f088a749f2ba453
Nicolas Moreira Ferreira <126179384+nicolasmoreiraferreira@users.noreply.github.com>
2023-08-03 14:42:34 -0300

    safe

--- aula14ex/ex016/script.js
+++ aula14ex/ex016/script.js
@@ -1,10 +1,28 @@
 function contar() {
-var inicio = document.getElementById('iinicio')
-var fim = document.getElementById('ifim')
-var passo = document.getElementById('ipasso')
+var inicio = document.getElementById('iinicio').value
+var fim = document.getElementById('ifim').value
+var passo = document.getElementById('ipasso').value
 var res = document.querySelector('div#res')
+let c = Number(inicio)
+console.log('c', c)
+console.log('inicio', inicio)
+if (Number(inicio) >= 0 && Number(fim) <= Number(inicio)) {
+  window.alert(`Errado`)
+  }
+  console.log('inicio', inicio)
+  if (inicio == '') {
+  res.innerHTML = `Impossivel contar`
+  return
+}
+  res.innerHTML = `Contando: <br>`
+  
+ for(c ; c < Number(fim) ; c= c+Number(passo))  {
+  console.log('c', c)
+  console.log('passo', passo)
+  res.innerHTML += `${c} `
+}
+}
 
-if (inicio.value >= 0 && Number(fim.value) <= inicio) {
-window.alert(`Errado`)
-}
-}
+//Pegar valor da var +
+
+//
