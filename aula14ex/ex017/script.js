54ce9bcf4194c70308bed3c43efd2c1b7d23722a
Nicolas Moreira Ferreira <126179384+nicolasmoreiraferreira@users.noreply.github.com>
2023-08-08 13:40:55 -0300

    safe

--- aula14ex/ex017/script.js
+++ aula14ex/ex017/script.js
@@ -1,8 +1,16 @@
 function tabuada() {
 let numero = Number(document.getElementById('inum').value)
 let res = Number(document.getElementById('res').value)
-
-switch (numero  )
+let ress = Number(document.getElementById('ires').value)
+console.log('numero', numero)
+console.group('ress', ress)
+switch (numero) {
+case 0:
+  res.innerHTML = numero * ress
+  break
+}
 
 
 }
+
+//Entender como multiplicar a let numero
